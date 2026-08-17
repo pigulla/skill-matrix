666e4fe1d8f41bdafe2acd76e23df3fe51926349
Raphael Pigulla <raphael@pigul.la>
2026-08-17 09:15:07 +0200

    ci: publish coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     reporters: ['default'],
     coverage: {
       provider: 'v8',
+      reportOnFailure: true,
       include: ['src/**/*.ts'],
       exclude: [
         '**/*.config.ts',
