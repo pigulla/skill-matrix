a69a78b7c43e4670350558d1659ae70737c1dec5
Raphael Pigulla <raphael@pigul.la>
2026-08-21 12:45:09 +0200

    refactor: remove now-unused dayjsSchema

--- src/util/dayjs.schema.ts
+++
@@ -1,6 +0,0 @@
-import dayjs, { type Dayjs } from 'dayjs'
-import z from 'zod'
-
-export const dayjsSchema = z
-  .custom<Dayjs>(value => dayjs.isDayjs(value), 'Expected a Dayjs instance')
-  .refine(value => value.isValid(), 'Expected a valid Dayjs instance')
